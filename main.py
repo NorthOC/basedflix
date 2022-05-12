7d48530eed2d137a79643b2f72b8a970b6cc37d3
NorthOC <rev.denisas@gmail.com>
2022-05-12 12:04:39 +0300

    added movie and media player selection

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 import tpb_scraper as scrape
 import streamer as stream
+import subprocess
 
-# parsed from apibay as a dictionary
+# clear console
+subprocess.run("clear")
+
+# parse data from apibay as a dictionary
 parsed_json = scrape.parse_me_matey()
 
-# transformed to array
+# transform data from dictionary to array
 parsed_array = scrape.info_to_arr(parsed_json)
 
-#available players to use
-# airplay     Apple TV
-# chromecast  Google Chromecast
-# dlna        DNLA
-# mplayer     MPlayer
-# mpv         MPV
-# omx         OMX
-# vlc         VLC
-# iina        IINA
-# smplayer    SMPlayer
-# xbmc        XBMC
-# stdout      Standard out (implies --quiet)
+# select a movie
+info_hash = stream.select_movie(parsed_array)
 
-magnet = parsed_array[0].split(", ")[1]
-print(magnet)
+# select a media player
+selected_player = stream.select_player()
 
-#TODO install Node and webtorrent-cli
-#stream.stream_me_matey(magnet=magnet)
-
-
-
-#magnet:?xt=urn:btih:8148C169C4B84048FCF8D44AAF275F82BC33829E&dn=&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce
-#magnet:?xt=urn:btih:EACABB7AC482AB0A7D04847E79A41BD9B5ECEAED&dn=&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce
-#magnet:?xt=urn:btih:1DB13C6F3A821F777405F9474CD1D3D2BE1FD2BE&dn=&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce
+# stream how much you want :)
+stream.stream_me_matey(info_hash, selected_player)
