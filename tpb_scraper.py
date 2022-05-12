02faed3b97443803301533d6606209d7d0621bff
NorthOC <rev.denisas@gmail.com>
2022-05-12 12:06:59 +0300

    moved comment above function and formatted string

--- tpb_scraper.py
+++ tpb_scraper.py
@@ -1,16 +1,17 @@
 import requests
 import json
 
+# scrape json api
 def parse_me_matey():
-    # scrape json api
     r = requests.get('https://apibay.org/precompiled/data_top100_207.json')
     content = r.content
     parsed = json.loads(content)
     return parsed
 
+
 # output easily manageable array
 def info_to_arr(parsed):
-    urls = []
+    movies = []
     counter = 1
 
     for torrent in parsed:
@@ -21,6 +22,7 @@ def info_to_arr(parsed):
         seeders = torrent["seeders"]
         leechers = torrent["leechers"]
 
+        # format size of movie
         if size_bytes > 1000000000:
             prefixer = "GiB" 
             size_xib = size_bytes / 1024 / 1024 / 1024
@@ -28,12 +30,15 @@ def info_to_arr(parsed):
             prefixer = "MiB"
             size_xib = size_bytes / 1024 / 1024
 
-        print(str(counter) + ".", name, str("%.2f" % size_xib) + " " + prefixer, seeders, leechers)
+        print((str(counter) + ".").ljust(4), name, str("%.2f" % size_xib) + " " + prefixer, seeders, leechers)
         counter += 1
 
-        # item is stored to array as id | magnet hash | name | size | seeders | leechers
+        # movie is stored to array as id | magnet hash | name | size | seeders | leechers
         item = str(torrent_id) + ", " + magnet + ", " + name + ", "\
              + str(size_bytes) + ", " + str(seeders) + ", " + str(leechers)
-        urls.append(item)
 
-    return urls
+        movies.append(item)
+    
+    print()
+
+    return movies
