c1090ae517fb29b78f9b2aac645ee4719d4c511a
NorthOC <rev.denisas@gmail.com>
2022-05-13 19:21:49 +0300

    made the installation process simpler, cleaner and more sane

--- install.sh
+++ install.sh
@@ -3,43 +3,30 @@
 # to stop script at errors
 set -eu
 
+if [ $(which apt-get) ]; then
+  echo "installing dependencies for Ubuntu..."
+  sudo apt-get -y install python3 python3-dev python3-pip npm
+  sudo npm install webtorrent-cli -g
+  sudo pip3 install virtualenv
+ 
+else
+  echo "*** You'll need to install Ubuntu or get a working venv for python yourself ***"
+fi
+
 echo "Creating virtual python environment (venv)..."
-python3 -m venv venv
+virtualenv venv
 
 echo "Installing packages to virtual environment from requirements.txt..."
 source ./venv/bin/activate
-pip install -r requirements.txt
-deactivate
+pip install --upgrade pip
+pip install --upgrade -r requirements.txt
 
-# directory of the repo folder on your computer
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "making systemwide symlink in /usr/local/bin"
+sudo ln -sf $(pwd)/basedflix /usr/local/bin/basedflix
 
-echo "setting the location of python files..."
-echo "adding location to basedflix.sh as: $SCRIPT_DIR"
 
-cat << EOF > $SCRIPT_DIR/basedflix.sh
-#!/bin/bash
-cd $SCRIPT_DIR
-source ./venv/bin/activate
-python3 main.py
-EOF
-
-#feel free to modify these parts below
-echo "basedflix.sh file updated"
-echo "checking or creating ~/.local/bin"
-
-mkdir -p ~/.local/bin
-
-echo "adding (export PATH= ~/.local/bin) to .bashrc"
-
-# remove or set a specific path you want
-cat << EOF >> ~/.bashrc
-export PATH="\$PATH:$HOME/.local/bin"
-EOF
-
-# set whatever directory you want to copy the basedflix file to
-echo "copying script basedflix.sh to ~/.local/bin"
-cp $SCRIPT_DIR/basedflix.sh ~/.local/bin/basedflix
-
-source ~/.bashrc
 echo "All done! Now you can type basedflix to start the program!"
+echo "Check out README for more info on supported players"
+echo
+echo "                                    ~NorthOC"
+echo
