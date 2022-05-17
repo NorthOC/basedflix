800375d5cc34598708bcebaf58983f1bd7dd457b
NorthOC <rev.denisas@gmail.com>
2022-05-17 15:42:58 +0300

    added search

---
+++ src/search_or_top.py
@@ -0,0 +1,16 @@
+def search_or_top():
+    answers = ["1", "2"]
+    query = ""
+
+    print("1. Current top 100 movies")
+    print("2. Search for a movie")
+
+    x = input(f"Enter \'{answers[0]}\' to see top movies or \'{answers[1]}\' to search for a movie: ")
+    
+    while x not in answers:
+        x = input("Wrong input. Try again:")
+
+    if x == answers[0]:
+        query = input("Enter the movie title:")
+
+    return query
